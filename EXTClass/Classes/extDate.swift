79a5e4e5bbdeab6ab3940e178434badd20a42546
Matias Correnti <saitco.pro@gmail.com>
2017-03-19 02:14:49 -0300

    Agregada extDate - Modificado extString

---
+++ EXTClass/Classes/extDate.swift
@@ -0,0 +1,135 @@
+//
+//  extDate.swift
+//  Pods
+//
+//  Created by Matías Correnti on 18/3/17.
+//
+//
+
+import UIKit
+
+extension Date {
+    
+    /// Lo convierte a String.
+    ///
+    /// - Parameters:
+    ///   - format: Formato a usar (Default: dd/MM/yyyy, HH:mm:ss)
+    /// - Returns: String.
+    public func toString(format: String = "dd/MM/yyyy, HH:mm:ss") -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+    
+    /// Optiene el año en curso.
+    ///
+    /// - Returns: Int.
+    public func getAño() -> Int {
+        let calendar =  Calendar.current
+        let res = calendar.component(.year, from: self)
+        return res
+    }
+    
+    /// Optiene el mes en curso.
+    ///
+    /// - Returns: Int.
+    public func getMes() -> Int {
+        let calendar =  Calendar.current
+        let res = calendar.component(.month, from: self)
+        return res
+    }
+    
+    /// Optiene el dia en curso.
+    ///
+    /// - Returns: Int.
+    public func getDia() -> Int {
+        let calendar =  Calendar.current
+        let res = calendar.component(.day, from: self)
+        return res
+    }
+    
+    /// Crea una fecha en base a un día y mes con el año en curso.
+    ///
+    /// - Parameters:
+    ///   - dia: Número del día.
+    ///   - mes: Número del mes.
+    /// - Returns: Date.
+    public func getFecha(_ dia: Int, mes: Int) -> Date {
+        let calendar =  Calendar.current
+        let year =  calendar.component(.year, from: self)
+        
+        let format: String = "dd/MM/yyyy"
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.date(from: "\(dia)/\(mes)/\(year)")!
+    }
+    
+    /// Compara la fecha con otra fecha para saber si son iguales (No tiene en cuenta la hora).
+    ///
+    /// - Parameter comparar: Fecha para comparar.
+    /// - Returns: true o false
+    func compararFechas(comparar: Date) -> Bool {
+        let calendar =  Calendar.current
+        let fechaSelf: Date = self
+        let year1: Int = calendar.component(.year, from: fechaSelf)
+        let month1: Int = calendar.component(.month, from: fechaSelf)
+        let day1: Int = calendar.component(.day, from: fechaSelf)
+        
+        let calendar2 = Calendar.current
+        let year2: Int = calendar2.component(.year, from: comparar)
+        let month2: Int = calendar2.component(.month, from: comparar)
+        let day2: Int = calendar2.component(.day, from: comparar)
+        
+        if day1 == day2 && month1 == month2 && year1 == year2 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    /// Unidades de tiempo disponibles.
+    public enum unidadTiempo {
+        case segundos
+        case minutos
+        case horas
+        case dias
+        case semanas
+        case meses
+        case años
+    }
+    
+    /// Pregunta si ya pasó el tiempo.
+    ///
+    /// - Parameters:
+    ///   - tiempo: Tiempo que debe pasar.
+    ///   - unidad: Unidad de tiempo a ver.
+    /// - Returns: True = Pasó el tiempo, False: No pasó el tiempo.
+    public func pasoElTiempo(tiempo: Int, unidad: unidadTiempo) -> Bool {
+        let seconds = abs(Date().timeIntervalSince1970 - self.timeIntervalSince1970)
+        let minutes = Int(floor(seconds / 60))
+        let hours = minutes / 60
+        let days = hours / 24
+        let weeks = days / 7
+        let months = weeks / 4
+        let years = months / 12
+        
+        switch unidad {
+        case .segundos:
+            if Int(seconds) > tiempo { return true }
+        case .minutos:
+            if minutes > tiempo { return true }
+        case .horas:
+            if hours > tiempo { return true }
+        case .dias:
+            if days > tiempo { return true }
+        case .semanas:
+            if weeks > tiempo { return true }
+        case .meses:
+            if months > tiempo { return true }
+        case .años:
+            if years > tiempo { return true }
+        }
+        return false
+    }
+    
+}
--- EXTClass/Classes/extString.swift
+++ EXTClass/Classes/extString.swift
@@ -10,7 +10,56 @@ import UIKit
 
 extension String {
     
-    func rangesOfString(_ s: String, textoExacto: Bool) -> [Range<Index>] {
+    /// Borra o reemplaza una cadena de texto dentro de otro texto.
+    ///
+    /// - Parameters:
+    ///   - original: Texto donde buscar.
+    ///   - buscar: Texto a buscar.
+    ///   - reemplazo: Texto con el cual reemplazar el texto buscado si se lo encuentra.
+    /// - Returns: Texto nuevo modificado.
+    public func borrar(expresionRegular buscar: String, reemplazo: String = "") -> String {
+        var texto: String = self
+        let ss = texto.matchesForRegexInText("\(buscar)")
+        if ss.count > 0 {
+            for itemR in ss {
+                if itemR != "" {
+                    let rangos = texto.rangesOfString(itemR, textoExacto: false)
+                    let dd = rangos.reversed()
+                    for itemG in dd {
+                        let nsRango = texto.nsRange(from: itemG)
+                        
+                        let nsString = texto as NSString
+                        texto = nsString.replacingCharacters(in: nsRango, with: reemplazo)
+                    }
+                }
+            }
+        }
+        return texto
+    }
+    
+    /// Busca mediante una expresion regular dentro de la cadena.
+    ///
+    /// - Parameter regex: Expresion regular usada para buscar.
+    /// - Returns: Coincidencias encontradas.
+    public func matchesForRegexInText(_ regex: String!) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [])
+            let nsString = self as NSString
+            let results = regex.matches(in: self, options: [], range: NSMakeRange(0, nsString.length))
+            return results.map { nsString.substring(with: $0.range)}
+        } catch let error as NSError {
+            NSLog("Invalid regex: %@", error.localizedDescription)
+            return []
+        }
+    }
+    
+    /// <#Description#>
+    ///
+    /// - Parameters:
+    ///   - s: <#s description#>
+    ///   - textoExacto: <#textoExacto description#>
+    /// - Returns: <#return value description#>
+    public func rangesOfString(_ s: String, textoExacto: Bool) -> [Range<Index>] {
         if textoExacto {
             let re = try! NSRegularExpression(pattern: NSRegularExpression.escapedPattern(for: s), options: [])
             return re.matches(in: self, options: [], range: nsRange(from: startIndex ..< endIndex)).flatMap { range(from: $0.range) }
@@ -21,7 +70,12 @@ extension String {
     }
     
     //Para Swift3
-    func range(from nsRange: NSRange) -> Range<String.Index>? {
+    
+    /// <#Description#>
+    ///
+    /// - Parameter nsRange: <#nsRange description#>
+    /// - Returns: <#return value description#>
+    public func range(from nsRange: NSRange) -> Range<String.Index>? {
         guard
             let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
             let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
@@ -32,12 +86,103 @@ extension String {
     }
     
     //Para Swift3
-    func nsRange(from range: Range<String.Index>) -> NSRange {
+    
+    /// <#Description#>
+    ///
+    /// - Parameter range: <#range description#>
+    /// - Returns: <#return value description#>
+    public func nsRange(from range: Range<String.Index>) -> NSRange {
         let utf16view = self.utf16
         let from = range.lowerBound.samePosition(in: utf16view)
         let to = range.upperBound.samePosition(in: utf16view)
         return NSMakeRange(utf16view.distance(from: utf16view.startIndex, to: from),
                            utf16view.distance(from: from, to: to))
+    }
+    
+    
+    /// Elimina todos los espacios en blanco que se encuentran al final de una cadena.
+    ///
+    /// - Returns: La cadena sin los espacios al final.
+    public func removeLastsEspacios() -> String {
+        //TODO: Mejorar
+        var returns = self
+        var analizar = true
+        while analizar {
+            if returns.characters.last == " " {
+                returns = returns.substring(with: returns.startIndex..<returns.characters.index(before: returns.endIndex))
+            } else {
+                analizar = false
+            }
+        }
+        return returns
+    }
+    
+    /// Convierte una cadena con el formato "3:4" a un CGFloat (Usar para los NSLayoutConstraint aspect de las imagenes).
+    ///
+    /// - Returns: Su equivalente en CGFloat.
+    public func aspectToCGFloat() -> CGFloat {
+        let valor = self.components(separatedBy: ":")
+        return CGFloat((valor[0] as NSString).floatValue)/CGFloat((valor[1] as NSString).floatValue)
+    }
+    
+    /// Largo del String.
+    public var length: Int {
+        return self.characters.count
+    }
+    
+    /// Buscar mediante index.
+    ///
+    /// - Parameter i: Posición a buscar.
+    subscript (i: Int) -> Character {
+        return self[self.characters.index(self.startIndex, offsetBy: i)]
+    }
+    
+    /// Buscar mediante index.
+    ///
+    /// - Parameter i: Posición a buscar.
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    /// Convierte un string a NSDate, si no puede regresa la fecha e hoy.
+    ///
+    /// - Returns: Un NSDate.
+    public func convertirAFecha() -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy, HH:mm:ss"
+        if let fechaSalida = formatter.date(from: self) {
+            return fechaSalida
+        }
+        return Date()
+    }
+    
+    /// Aplica formato personalizado a la cadena (3 a 03).
+    ///
+    /// - Returns: Cadena modificada.
+    public func MyFormato () -> String {
+        do {
+            let valor: Int = try Int(self)!
+            guard valor <= 9 else { return self }
+            return String(format: "%02d", valor)
+        } catch {
+            return self
+        }
+    }
+    
+    /// Convierte el String con el formato '0|0|0|0' en una lista de Int.
+    ///
+    /// - Parameter separador: Caracter usado para separar los numeros (Defauld: |).
+    /// - Returns: [Int].
+    public func descomponer(separador: String = "|") -> [Int] { //converALibros
+        return self.components(separatedBy: separador).map { Int($0)! }
+    }
+    
+    /// Convierte en Date el String.
+    ///
+    /// - Returns: Date.
+    public func getFecha() -> Date {
+        let fechaInt = self.descomponer(separador: "|")
+        return Date().getFecha(fechaInt[1], mes: fechaInt[0])
     }
     
 }
